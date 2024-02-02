494c48e3f66692c8df6b6c9d52fbb7496022520c
Rahul Taneja <rahultaneja03@gmail.com>
2024-02-03 01:18:13 +0530

    caching of get API

--- src/app/crud.service.ts
+++ src/app/crud.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CrudService {
   private apiUrl = 'https://jsonplaceholder.typicode.com';
+  private cache: { [key: string]: any[] } = {}; 
 
   constructor(private http: HttpClient) {}
 
-  getItems(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/posts`);
+ getItems(): Observable<any[]> {
+    const cacheKey = 'getItems';
+
+    if (this.cache[cacheKey]) {
+      return of(this.cache[cacheKey]);
+    } else {
+      return this.http.get<any[]>(`${this.apiUrl}/posts`).pipe(
+        catchError(error => {
+          console.error('Error fetching items:', error);
+          return of([]); // Return an empty array on error
+        })
+      );
+    }
   }
 
   addItem(item: any): Observable<any> {
